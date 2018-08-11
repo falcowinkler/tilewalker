ead12f5133156150a70441f9c102789989e71827
Falco Winkler <falco.winkler@esailors.de>
2018-08-11 11:17:00 +0000

    initial

---
+++ crawler.py
@@ -0,0 +1,83 @@
+import urllib.request
+import urllib.error
+from bs4 import BeautifulSoup
+import csv
+import re
+
+
+def save_config(current_num):
+    with open("config", 'w') as config_file:
+        config_file.write(str(current_num))
+
+
+def get_current_number():
+    try:
+        with open("config") as config_file:
+            current_number = int(config_file.readlines()[0])
+    except FileNotFoundError:
+        current_number = 1
+        save_config(current_number)
+    return current_number
+
+
+def scrape(current_number):
+    url = "http://www.nmaps.net/" + str(current_number)
+    try:
+        content = urllib.request.urlopen(url)
+    except urllib.error.HTTPError:
+        return None, None
+
+    soup = BeautifulSoup(content, 'html.parser')
+
+    data = soup.find("textarea", attrs={'rows': '1'})
+    if data is not None:
+        data = data.contents[0]
+    else:
+        return None, None
+
+    tr_s = soup.find("table", attrs={'class': 'formtable'}).find_all("tr")
+    tags = []
+
+    for x in tr_s[1].td.find_all("a"):
+        if len(x.contents) > 0:
+            tags.append(x.contents[0])
+
+    rating = None
+    for i in range(1, 6):
+        rating = soup.find("div", attrs={'class': 'rated' + str(i)})
+        if rating is not None:
+            rating = rating.contents[0]
+            break
+
+    if rating is not None:
+        tags.append("rating:" + str(rating))
+
+    tags.append("id:" + str(current_number))
+    return data, tags
+
+
+def persist(data, labels, csvfile):
+    spamwriter = csv.writer(csvfile, delimiter=';')
+    spamwriter.writerow((data, ",".join(labels)))
+
+
+def data_in_expected_format(data):
+    try:
+        re.match(".*#.*#.*#([0-9A-Q@;:y=?<>]{713})|", data).group(1)
+        return True
+    except:
+        return False
+
+
+if __name__ == '__main__':
+    current_no = get_current_number()
+    with open("numa_archive.csv", "a", newline="") as csvfile:
+        while current_no < 300000:
+            data, labels = scrape(current_no)
+            if data is not None:
+                data = data.replace('\n', ' ').replace('\r', '')
+                if data_in_expected_format(data):
+                    print("Crawled map " + str(current_no))
+                    persist(data, labels, csvfile)
+            current_no += 1
+            save_config(current_no)
---
+++ tests/crawler_tests.py
@@ -0,0 +1,32 @@
+import unittest
+from crawler import *
+import os
+
+class Scraper_tests(unittest.TestCase):
+    test_data = "$Thwumpwards IV#Hindi#minimalism#00000000000000000000000000000000000000000000000000000000000000000\
+000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000|5^396,540!8^420,5\
+40,3!8^372,540,3!11^396,444,396,156!8^396,516,3!0^396,60#"
+
+    def test_scrape(self):
+        data, labels = scrape(238130)
+        self.assertEqual(data, self.test_data)
+        self.assertEqual(labels, ["author:hindi", "minimalism", "symmetrical", "thwumpwards", "unrated", "id:238130"])
+
+    def test_persist(self):
+        with open("numa_archive.csv", 'a') as csv_file:
+            persist("leveldata", ["label1", "label2"], csv_file)
+        with open("numa_archive.csv") as csv_file:
+            firstline = csv_file.readlines()[0]
+            self.assertEqual(firstline.rstrip(), "leveldata;label1,label2")
+
+    def test_data_in_expected_format(self):
+        self.assertTrue(data_in_expected_format(self.test_data))
+        self.assertFalse(data_in_expected_format(self.test_data[20:]))
+
+    def tearDown(self):
+        os.remove("numa_archive.csv")
